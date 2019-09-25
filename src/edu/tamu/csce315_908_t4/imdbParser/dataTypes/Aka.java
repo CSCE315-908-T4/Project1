c56dcd0ce7262e0249007bc8876042a0f0582f3e
Buzzec <bbetter9101@gmail.com>
2019-09-25 16:11:45 -0500

    Added more data types
    Signed-off-by: Buzzec <bbetter9101@gmail.com>

---
+++ src/edu/tamu/csce315_908_t4/imdbParser/dataTypes/Aka.java
@@ -0,0 +1,10 @@
+package edu.tamu.csce315_908_t4.imdbParser.dataTypes;
+
+public class Aka{
+    private String tconst;
+    private int ordering;
+    private String title;
+    private String region;
+    private String language;
+    private boolean isOriginalTitle;
+}
---
+++ src/edu/tamu/csce315_908_t4/imdbParser/dataTypes/TitleBasics.java
@@ -0,0 +1,12 @@
+package edu.tamu.csce315_908_t4.imdbParser.dataTypes;
+
+public class TitleBasics{
+    private String tconst;
+    private String titleType;
+    private String primaryTitle;
+    private String originalTitle;
+    private boolean isAdult;
+    private short startYear;
+    private short endYear;
+    private int runtimeMinutes;
+}
