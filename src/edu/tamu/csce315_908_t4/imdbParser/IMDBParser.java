f075388e0f48ff6fb917013ea038aaa251c49458
Buzzec <bbetter9101@gmail.com>
2019-09-25 15:35:16 -0500

    Began java code

---
+++ src/edu/tamu/csce315_908_t4/imdbParser/IMDBParser.java
@@ -0,0 +1,5 @@
+package edu.tamu.csce315_908_t4.imdbParser;
+
+public class IMDBParser{
+
+}
---
+++ src/edu/tamu/csce315_908_t4/imdbParser/dataTypes/People.java
@@ -0,0 +1,8 @@
+package edu.tamu.csce315_908_t4.imdbParser.dataTypes;
+
+public class People{
+    private String nconst;
+    private String primaryName;
+    private short birthYear;
+    private short deathYear;
+}
