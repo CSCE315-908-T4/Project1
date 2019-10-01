21d2885c553767c99e46a51a28cc9ba188e89dba
Buzzec <bbetter9101@gmail.com>
2019-10-01 14:09:00 -0500

    Added sql test
    Signed-off-by: Buzzec <bbetter9101@gmail.com>

---
+++ src/edu/tamu/csce315_908_t4/sqlguiTest/dbSetup.java
@@ -0,0 +1,12 @@
+package edu.tamu.csce315_908_t4.sqlguiTest;
+
+/*
+Robert Lightfoot, Tim McGuire
+CSCE 315
+Database setup example
+Purpose: Keeping usernames and password private.
+*/
+public final class dbSetup{
+    public static final String user = "Buzzec";
+    public static final String pswd = "#E?Ez>wCg=x+-#sDn2xA*rq33";
+}//end class
