f53af962fbb35b2377dde64209b6e2b0423d9125
JonathanLabrum <jonathan.d.labrum@gmail.com>
2019-10-10 13:24:46 -0500

    made SeparationWindow
    Signed-off-by: JonathanLabrum <jonathan.d.labrum@gmail.com>

---
+++ src/edu/tamu/csce315_908_t4/gui/frontend/SeparationWindow.java
@@ -0,0 +1,11 @@
+package edu.tamu.csce315_908_t4.gui.frontend;
+
+import javafx.scene.Scene;
+
+public class SeparationWindow implements IWindow {
+
+    @Override
+    public Scene getScene() {
+        return null;
+    }
+}
