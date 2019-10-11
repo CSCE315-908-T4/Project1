604f7fc67a5a71fd22cc8b1e4de3b3e8c1f4f5d7
Abeer Zaidi <abeer.zaidi97@hotmail.com>
2019-10-11 14:47:00 -0500

    Update RecommendationWindow.java

--- src/edu/tamu/csce315_908_t4/gui/frontend/RecommendationWindow.java
+++ src/edu/tamu/csce315_908_t4/gui/frontend/RecommendationWindow.java
@@ -25,11 +25,11 @@ public class RecommendationWindow implements IWindow {
     {
         primaryStage.setTitle("FILMPEDIA recommendations");
 
-        TextField Name = new TextField();
-        TextField Year = new TextField();
-        TextField Genre = new TextField();
-        Button Back = new Button("Back");
-        Button Search = new Button("Generate Recommendations");
+        TextField name = new TextField();
+        TextField year = new TextField();
+        TextField genre = new TextField();
+        Button back = new Button("Back");
+        Button search = new Button("Generate Recommendations");
         GridPane page = new GridPane();
         TableView table = new TableView();
         TableColumn<String, Person> nameColumn = new TableColumn<>("Actor Name");
@@ -38,18 +38,18 @@ public class RecommendationWindow implements IWindow {
         table.getColumns().add(titleColumn);
         table.getColumns().add(yearColumn);
         table.getColumns().add(nameColumn);
-        page.getChildren().add(Name, 0,0,2,1 );
-        page.getChildren().add(Year, 1,0,2,1);
-        page.getChildren().add(Genre, 2,0,2,1);
-        page.getChildren().add(Back, 0, 9, 1, 1);
-        page.getChildren().add(Search, 9,9,1,1);
+        page.getChildren().add(name, 0,0,2,1 );
+        page.getChildren().add(year, 1,0,2,1);
+        page.getChildren().add(genre, 2,0,2,1);
+        page.getChildren().add(back, 0, 9, 1, 1);
+        page.getChildren().add(search, 9,9,1,1);
         page.setHgap(10);
         page.setVgap(10);
         Scene scene = new Scene(page, 300,400);
         primaryStage.setScene(scene);
         primaryStage.show();
-        Search.setOnAction(event -> {
-            VBox vbox = new VBox(Back, table);
+        search.setOnAction(event -> {
+            VBox vbox = new VBox(back, table);
             Scene scene1 = new Scene(vbox, 150, 200);
             primaryStage.setScene(scene1);
             primaryStage.show();
