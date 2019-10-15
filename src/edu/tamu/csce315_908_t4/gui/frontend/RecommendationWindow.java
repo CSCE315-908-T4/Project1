d3ff625ecfe9dd97ad8548a1af11fe16951cd65a
Abeer Zaidi <abeer.zaidi97@hotmail.com>
2019-10-15 13:55:19 -0500

    Update RecommendationWindow.java

--- src/edu/tamu/csce315_908_t4/gui/frontend/RecommendationWindow.java
+++ src/edu/tamu/csce315_908_t4/gui/frontend/RecommendationWindow.java
@@ -14,6 +14,7 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.awt.event.ActionEvent;
 import java.util.ArrayList;
 
 public class RecommendationWindow implements IWindow {
@@ -32,24 +33,25 @@ public class RecommendationWindow implements IWindow {
             Button search = new Button("Generate Recommendations");
             back.setOnAction(frontend::backAction);
             TableView table = new TableView();
-            search.setOnAction(event -> {
-                //set up arguments for search
-                String actor = name.getText();
-                Nconst actorNconst = new Nconst(actor);
-                String Genre = genre.getText();
-                int Year = Integer.parseInt(year.getText());
-                RecommendationArg recommendationArgs= new RecommendationArg(actorNconst, Genre, Year);
-                //do search
-                ArrayList<RecommendationResult> results = backend.getRecommendations(recommendationArgs);
-                //populate table;
-                int i;
-                for(i = 0; i<5; i++){
-                    table.getItems().add(new TableElements(results.get(i).movieTitle, results.get(i).year,results.get(i).actorName));
-                }
-                //show scene
-                VBox vbox = new VBox(table, back);
-                scene = new Scene(vbox, 300, 400);
-            });
+            search.setOnAction(this::searchAction);
+//            search.setOnAction(event -> {
+//                //set up arguments for search
+//                String actor = name.getText();
+//                Nconst actorNconst = new Nconst(actor);
+//                String Genre = genre.getText();
+//                int Year = Integer.parseInt(year.getText());
+//                RecommendationArg recommendationArgs= new RecommendationArg(actorNconst, Genre, Year);
+//                //do search
+//                ArrayList<RecommendationResult> results = backend.getRecommendations(recommendationArgs);
+//                //populate table;
+//                int i;
+//                for(i = 0; i<5; i++){
+//                    table.getItems().add(new TableElements(results.get(i).movieTitle, results.get(i).year,results.get(i).actorName));
+//                }
+//                //show scene
+//                VBox vbox = new VBox(table, back);
+//                scene = new Scene(vbox, 300, 400);
+//            });
             page = new GridPane();
             TableColumn<String, TableElements> nameColumn = new TableColumn<>("Actor Name");
             TableColumn<Integer, TableElements> yearColumn = new TableColumn<>("Year");
@@ -68,17 +70,15 @@ public class RecommendationWindow implements IWindow {
             scene = new Scene(page, 800,600);
         }
 
+    private void searchAction(javafx.event.ActionEvent actionEvent) {
+    }
+
     class TableElements {
 
-        private String Title = null;
-        private int Year = 0;
-        private String Name = null;
+        public String Title = null;
+        public int Year = 0;
+        public String Name = null;
 
-        public TableElements() {
-            Title = null;
-            Year = 0;
-            Name= null;
-        }
 
         public TableElements(String Title, int Year, String Name) {
             this.Title = Title;
@@ -94,7 +94,6 @@ public class RecommendationWindow implements IWindow {
     public Scene getScene() {
         return scene;
     }
-
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setScene(getScene());
         primaryStage.setTitle("FILMPEDIA Recommendations");
