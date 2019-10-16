e1d72140f85c17c82d6730977e7e16cadc534f05
Abeer Zaidi <abeer.zaidi97@hotmail.com>
2019-10-16 16:22:26 -0500

    Update RecommendationWindow.java

--- src/edu/tamu/csce315_908_t4/gui/frontend/RecommendationWindow.java
+++ src/edu/tamu/csce315_908_t4/gui/frontend/RecommendationWindow.java
@@ -12,16 +12,28 @@ import javafx.stage.Stage;
 
 import java.awt.event.ActionEvent;
 import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
 
 public class RecommendationWindow implements IWindow {
 
+        boolean isSearching;
+        private Frontend frontend;
+        private IBackend backend;
         private Scene scene;
         public GridPane page;
-        public RecommendationWindow(Frontend frontend, IBackend backend)
+        private ExecutorService executorService;
+        private TextField name;
+        private TextField year;
+        private TextField genre;
+        public RecommendationWindow(Frontend frontend, IBackend backend, ExecutorService executorService)
         {
-            TextField name = new TextField();
-            TextField year = new TextField();
-            TextField genre = new TextField();
+            isSearching = true;
+            this.frontend = frontend;
+            this.backend = backend;
+            this.executorService = executorService;
+            name = new TextField();
+            year = new TextField();
+            genre = new TextField();
             Label label1 = new Label("Actor");
             Label label2 = new Label("Genre");
             Label label3 = new Label("Year");
@@ -30,24 +42,6 @@ public class RecommendationWindow implements IWindow {
             back.setOnAction(frontend::backAction);
             TableView table = new TableView();
             search.setOnAction(this::searchAction);
-//            search.setOnAction(event -> {
-//                //set up arguments for search
-//                String actor = name.getText();
-//                Nconst actorNconst = new Nconst(actor);
-//                String Genre = genre.getText();
-//                int Year = Integer.parseInt(year.getText());
-//                RecommendationArg recommendationArgs= new RecommendationArg(actorNconst, Genre, Year);
-//                //do search
-//                ArrayList<RecommendationResult> results = backend.getRecommendations(recommendationArgs);
-//                //populate table;
-//                int i;
-//                for(i = 0; i<5; i++){
-//                    table.getItems().add(new TableElements(results.get(i).movieTitle, results.get(i).year,results.get(i).actorName));
-//                }
-//                //show scene
-//                VBox vbox = new VBox(table, back);
-//                scene = new Scene(vbox, 300, 400);
-//            });
             page = new GridPane();
             TableColumn<String, TableElements> nameColumn = new TableColumn<>("Actor Name");
             TableColumn<Integer, TableElements> yearColumn = new TableColumn<>("Year");
@@ -68,6 +62,23 @@ public class RecommendationWindow implements IWindow {
 
 
     private void searchAction(javafx.event.ActionEvent actionEvent) {
+//                //set up arguments for search
+//                String actor = name.getText();
+//                Nconst actorNconst = new Nconst(actor);
+//                String Genre = genre.getText();
+//                int Year = Integer.parseInt(year.getText());
+//                RecommendationArg recommendationArgs= new RecommendationArg(actorNconst, Genre, Year);
+//                //do search
+//                ArrayList<RecommendationResult> results = backend.getRecommendations(recommendationArgs);
+//                //populate table;
+//                int i;
+//                for(i = 0; i<5; i++){
+//                    table.getItems().add(new TableElements(results.get(i).movieTitle, results.get(i).year,results.get(i).actorName));
+//                }
+//                //show scene
+//                VBox vbox = new VBox(table, back);
+//                scene = new Scene(vbox, 300, 400);
+//            });
     }
 
     public static void main(String[] args) {
