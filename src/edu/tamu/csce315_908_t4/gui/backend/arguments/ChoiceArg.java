0ed24621237095988bb7843ba9c28351df610001
JonathanLabrum <jonathan.d.labrum@gmail.com>
2019-10-17 12:30:32 -0500

    updated ChoiceArg and ChoiceResult
    Signed-off-by: JonathanLabrum <jonathan.d.labrum@gmail.com>

--- src/edu/tamu/csce315_908_t4/gui/backend/arguments/ChoiceArg.java
+++ src/edu/tamu/csce315_908_t4/gui/backend/arguments/ChoiceArg.java
@@ -3,11 +3,9 @@ package edu.tamu.csce315_908_t4.gui.backend.arguments;
 import edu.tamu.csce315_908_t4.gui.backend.Tconst;
 
 public class ChoiceArg implements IArgument {
-    public final Tconst tconst;
-    //public final Tconst parentTconst;
-    //public final
+    public final Tconst seriesTitle;
 
-    public ChoiceArg(Tconst tconst) {
-        this.tconst = tconst;
+    public ChoiceArg(Tconst seriesTitle) {
+        this.seriesTitle = seriesTitle;
     }
 }
---
+++ src/edu/tamu/csce315_908_t4/gui/backend/result/ChoiceResult.java
@@ -0,0 +1,13 @@
+package edu.tamu.csce315_908_t4.gui.backend.result;
+
+import edu.tamu.csce315_908_t4.gui.backend.Tconst;
+
+public class ChoiceResult {
+    public final Tconst seriesTitle;
+    //public final ArrayList<> episodes;
+
+    public ChoiceResult(Tconst seriesTitle) {
+        this.seriesTitle = seriesTitle;
+        //this.episodes = episodes;
+    }
+}
